481aa49029d529a0a395f1c32e48bcf395d7d9f4
achref <achrefbs>
2020-09-09 18:38:21 +0100

    task 4 pep8

--- api/v1/views/index.py
+++ api/v1/views/index.py
@@ -10,18 +10,18 @@ from models import storage
 
 @app_views.route("/status")
 def status():
-	"""returns a JSON status"""
-	return jsonify({"status": "OK"})
+    """returns a JSON status"""
+    return jsonify({"status": "OK"})
 
 
 @app_views.route("/stats")
 def stats():
-	"""retrieves the number of each objects by type"""
-	stats = {}
-	stats["amenities"] = storage.count("Amenity")
-	stats["cities"] = storage.count("City")
-	stats["places"] = storage.count("Place")
-	stats["reviews"] = storage.count("Review")
-	stats["states"] = storage.count("State")
-	stats["users"] = storage.count("User")
-	return jsonify(stats)
+    """retrieves the number of each objects by type"""
+    stats = {}
+    stats["amenities"] = storage.count("Amenity")
+    stats["cities"] = storage.count("City")
+    stats["places"] = storage.count("Place")
+    stats["reviews"] = storage.count("Review")
+    stats["states"] = storage.count("State")
+    stats["users"] = storage.count("User")
+    return jsonify(stats)
